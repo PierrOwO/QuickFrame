6212605aa2925d018cbf6fc47bb1d76c86698ac1
Piotr Miłoś <piotrmilos@macbookair.local>
2025-12-12 22:42:08 +0100

    preparation for version 3.0.1

--- resources/js/app-select2.js
+++
@@ -1,2 +0,0 @@
-import './select2/4.1.0.min.js'
-import '../css/select2/4.1.0.min.css'
--- resources/js/auth.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-window.$ = $;
-window.jQuery = $;
-import axios from 'axios';
-window.axios = axios;
-axios.defaults.headers.common['X-CSRF-TOKEN'] =
-    document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-import './auth/login.js';
-import './auth/register.js';
